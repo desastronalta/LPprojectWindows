b4d58f7d07f8baecab761e3c6e6521b338fb89d2
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-07-06 12:31:37 -0300

    teste

--- LPInterfaces/src/view/Infos.java
+++ LPInterfaces/src/view/Infos.java
@@ -6,6 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Infos {
 	private static String nome;
@@ -55,22 +58,43 @@ public class Infos {
 		
 		JLabel Nome = new JLabel(this.nome);
 		Nome.setFont(new Font("Yu Gothic", Font.PLAIN, 16));
-		Nome.setBounds(29, 36, 348, 28);
+		Nome.setBounds(142, 36, 282, 28);
 		panel.add(Nome);
 		
 		JLabel Cpf = new JLabel(this.cpf);
 		Cpf.setFont(new Font("Yu Gothic", Font.PLAIN, 16));
-		Cpf.setBounds(29, 75, 348, 28);
+		Cpf.setBounds(132, 79, 292, 28);
 		panel.add(Cpf);
 		
 		JLabel Email = new JLabel(this.email);
 		Email.setFont(new Font("Yu Gothic", Font.PLAIN, 16));
-		Email.setBounds(29, 118, 348, 28);
+		Email.setBounds(132, 118, 235, 28);
 		panel.add(Email);
+		
+		JLabel lblNewLabel_2 = new JLabel("Email");
+		lblNewLabel_2.setBounds(46, 118, 46, 28);
+		panel.add(lblNewLabel_2);
 		
 		JLabel titulo = new JLabel("Informações");
 		titulo.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 20));
 		titulo.setBounds(132, 0, 141, 25);
 		panel.add(titulo);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setBounds(46, 36, 66, 28);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("CPF");
+		lblNewLabel_1.setBounds(46, 79, 46, 28);
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Voltar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton.setBounds(142, 195, 120, 36);
+		panel.add(btnNewButton);
 	}
 }
